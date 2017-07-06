35033b716f6cc4e7a7cfa989edb6dc9a3e482e63
craig <craig@zipari.com>
2017-07-06 17:31:43 -0400

    Added quickmail

---
+++ base/settings.py
@@ -0,0 +1,72 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'DSHFINRHN$O#45%)D89K@NGT)#NDO%_@NAJGMSMVNGTO!1u704'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+INSTALLED_APPS = ()
+MIDDLEWARE = ()
+TEMPLATES = ()
+
+# For email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = ''
+
+# logging
+LOG_FILE = os.environ.get('LOG_FILE', os.path.join(BASE_DIR, 'quickmail.log'))
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'basic': {
+            'format': '%(levelname)s %(asctime)s %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'basic'
+        },
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': LOG_FILE,
+            'formatter': 'basic'
+        },
+    },
+    'loggers': {
+        'mailer': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.10/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'America/New_York'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# This makes it so you can use a local settings file to override settings
+try:
+    from .local_settings import *
+except ImportError:
+    pass
---
+++ quickmail.py
@@ -0,0 +1,66 @@
+import argparse
+import sys
+import os
+
+if __name__ == "__main__":
+    if sys.version_info < (3, 5):
+        print('This script requires version 3.5+ of python. Please try running it with command `python3` instead')
+        exit(8)
+
+    parser = argparse.ArgumentParser(
+        description='Quick Mailer'
+    )
+    parser.add_argument('-m', '--message', dest='msg', type=str, required=True,
+                        help="The plain text message or filename of a message to send")
+    parser.add_argument('-t', '--to', dest='to', nargs='+', metavar='email@domain.com', type=str,
+                        help="Email address to recieve the message", required=True)
+    parser.add_argument('-f', '--from', dest='sender', type=str, help="The from Email. Must be enabled in the AWS config")
+    parser.add_argument('-s', '--subject', dest='subject', required=True, type=str, help='The subject line')
+    parser.add_argument('-w', '--html', dest='html', action='store_true', required=False,
+                        help='If using a file for m and file is html set this flag to use html email')
+
+    args = parser.parse_args()
+
+    # Here we inject the bulk db into settings and load django
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "base.settings")
+    try:
+        import django
+        from django.conf import settings
+    except ImportError:
+        django = None
+        settings = None
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        )
+
+    django.setup()
+    from django.core.mail import send_mail
+
+    msg = ''
+    is_file = False
+    if os.path.isfile(args.msg) is False:
+        msg = "{}".format(args.msg)
+    else:
+        try:
+            msg = open(args.msg).read()
+            is_file = True
+        except OSError as e:
+            print("Could not read msg file, exception said: {}".format(e))
+            exit(4)
+
+    sender = args.sender
+    if sender == '':
+        sender = settings.DEFAULT_FROM_EMAIL
+
+    if is_file and args.html:
+        html_msg = 'Multipart HTML Encoded message'
+        sent = send_mail(args.subject, html_msg, sender, args.to, fail_silently=False, html_message=msg)
+    else:
+        sent = send_mail(args.subject, msg, sender, args.to, fail_silently=False)
+
+    if sent:
+        print("Email sent successfully")
+    else:
+        print("There was an issue sending the message")

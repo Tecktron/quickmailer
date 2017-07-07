deb87bf61a4fbd819316c3d054a1769242f6ee55
Tecktron <git@tecktron.net>
2017-07-07 11:00:54 -0400

    Added plain text to html email. Some cleanup. Added examples to readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Quickmailer
-A quick command line based script to send SMTP email with django.
+A simple command line script to send SMTP email using [Django](https://www.djangoproject.com/) .
 
 ## Requirements:
 - Python 3.x
@@ -7,8 +7,18 @@ A quick command line based script to send SMTP email with django.
 - Access to an SMTP server
 
 ### Setup
-Update the `base/settings.py` file with the smtp server credentials.
+Update the `base/settings.py` file with your smtp server credentials.
 
 ### Run
 Simply run `python quickmail.py` from the command line.
 Use `-h` option for help.
+
+#### Examples
+
+**Simple Message:**
+
+`python3 quickmail.py -m "This is a test" -s "Testing" -t "you@example.com"`
+
+**HTML Message from a file:**
+
+`python3 quickmail.py -m /path/to/test_msg.html -w -s "Testing" -t "you@example.com"`
--- quickmail.py
+++ quickmail.py
@@ -1,30 +1,32 @@
 import argparse
-import sys
 import os
+import re
+import sys
 
 if __name__ == "__main__":
     if sys.version_info < (3, 0):
-        print('This script requires version 3.5+ of python. Please try running it with command `python3` instead')
+        print("This script requires version 3+ of python. Please try running it with command 'python3' instead")
         exit(8)
 
     parser = argparse.ArgumentParser(
-        description='Quick Mailer'
+        description="Quick Mailer"
     )
-    parser.add_argument('-m', '--message', dest='msg', type=str, required=True,
+    parser.add_argument("-m", "--message", dest="msg", type=str, required=True,
                         help="The plain text message or filename of a message to send")
-    parser.add_argument('-t', '--to', dest='to', nargs='+', metavar='email@domain.com', type=str,
+    parser.add_argument("-t", "--to", dest="to", nargs="+", metavar="email@domain.com", type=str,
                         help="Email address to recieve the message", required=True)
-    parser.add_argument('-f', '--from', dest='sender', type=str, required=False,
+    parser.add_argument("-f", "--from", dest="sender", type=str, required=False,
                         help="The from Email, if not provided, the settings will be used. NOTE: A specific address may "
                              "be required by your SMTP server")
-    parser.add_argument('-s', '--subject', dest='subject', required=True, type=str, help='The subject line')
-    parser.add_argument('-w', '--html', dest='html', action='store_true', required=False,
-                        help='If using a file for m and file is html set this flag to use html email')
+    parser.add_argument("-s", "--subject", dest="subject", required=True, type=str, help="The subject line")
+    parser.add_argument("-w", "--html", dest="html", action="store_true", required=False,
+                        help="If using a file for m and file is html set this flag to use html email")
 
     args = parser.parse_args()
 
-    # Here we inject the bulk db into settings and load django
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "base.settings")
+    # Here we inject the settings and load django
+    if not os.environ.get("DJANGO_SETTINGS_MODULE", False):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "base.settings")
     try:
         import django
         from django.conf import settings
@@ -38,9 +40,11 @@ if __name__ == "__main__":
         )
 
     django.setup()
+    # don't import Django things until after setup or errors abound
     from django.core.mail import send_mail
+    from django.utils.html import strip_tags
 
-    msg = ''
+    msg = ""
     is_file = False
     if os.path.isfile(args.msg) is False:
         msg = "{}".format(args.msg)
@@ -53,12 +57,17 @@ if __name__ == "__main__":
             exit(4)
 
     sender = args.sender
-    if sender == '':
+    if sender == "":
         sender = settings.DEFAULT_FROM_EMAIL
 
     if is_file and args.html:
-        html_msg = 'Multipart HTML Encoded message'
-        sent = send_mail(args.subject, html_msg, sender, args.to, fail_silently=False, html_message=msg)
+        # quick and dirty, create a plain text version.
+        # replace breaks and paragraphs with newlines
+        plain = re.sub("<br\s*?>", "\n", msg)
+        plain = re.sub("</p>", "\n\n", plain)
+        # strip the rest of the tags.
+        plain = strip_tags(plain)
+        sent = send_mail(args.subject, plain, sender, args.to, fail_silently=False, html_message=msg)
     else:
         sent = send_mail(args.subject, msg, sender, args.to, fail_silently=False)
 
